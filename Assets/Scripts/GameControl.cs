39a805409ec77c4257c9187eefd761eda676266f
poltato0404 <pauldp143@gmail.com>
2023-11-29 01:02:01 +0800

    MemoryGame

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    GameObject token;
+    List<int> faceIndexes = new List<int> { 0, 1, 2, 3, 0, 1, 2, 3 };
+
+    public static System.Random rnd = new System.Random();
+    public int shuffleNum = 0;
+    int[] visibleFaces = { -1, -2 };
+    int clicks = 0;
+
+    void Start()
+    {
+        int originalCnt = faceIndexes.Count;
+        float yPosition = 1.6f;
+        float xPosition = -2.2f;
+
+        for (int i = 0; i < 7; i++)
+        {
+            shuffleNum = rnd.Next(0, (faceIndexes.Count));
+            // Set card face index
+            var temp = Instantiate(token,
+                new Vector3(xPosition, yPosition, 0),
+                Quaternion.identity);
+            temp.GetComponent<MainToken>().faceIndex = faceIndexes[shuffleNum];
+            faceIndexes.Remove(faceIndexes[shuffleNum]);
+            xPosition = xPosition + 4;
+            if (i == (originalCnt / 2 - 2))
+            {
+                yPosition = -1.6f;
+                xPosition = -6.2f;
+            }
+        }
+        token.GetComponent<MainToken>().faceIndex = faceIndexes[0];
+    }
+
+    public bool TwoCardsUp()
+    {
+        bool cardsUp = false;
+        if (visibleFaces[0] >= 0 && visibleFaces[1] >= 0)
+        {
+            cardsUp = true;
+        }
+        return cardsUp;
+    }
+
+    public void AddVisibleFace(int index)
+    {
+        if (visibleFaces[0] == -1)
+        {
+
+            visibleFaces[0] = index;
+        }
+        else if (visibleFaces[1] == -2)
+        {
+            visibleFaces[1] = index;
+        }
+
+    }
+
+    public void RemoveVisibleFace(int index)
+    {
+        if (visibleFaces[0] == index)
+        {
+            visibleFaces[0] = -1;
+        }
+        else if (visibleFaces[1] == index)
+        {
+            visibleFaces[1] = -2;
+        }
+    }
+
+    public bool CheckMatch(int index)
+    {
+        bool success = false;
+        if (visibleFaces[0] == visibleFaces[1])
+        {
+            visibleFaces[0] = -1;
+            visibleFaces[1] = -2;
+            success = true;
+        }
+        return success;
+    }
+
+    void Awake()
+    {
+        token = GameObject.Find("Token");
+    }
+}
---
+++ Assets/Scripts/MainToken.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainToken : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    GameObject gameControl;
+
+    public Sprite[] faces;
+    public Sprite back;
+    public int faceIndex;
+    public bool matched = false;
+
+    public void OnMouseDown()
+    {
+        if (matched == false)
+        {
+            if (spriteRenderer.sprite == back)
+            {
+                if (gameControl.GetComponent<GameControl>().TwoCardsUp() == false)
+                {                  
+                    spriteRenderer.sprite = faces[faceIndex];
+                    gameControl.GetComponent<GameControl>().AddVisibleFace(faceIndex);
+                    matched = gameControl.GetComponent<GameControl>().CheckMatch(faceIndex);
+                }
+            }
+            else
+            {
+                spriteRenderer.sprite = back;
+                gameControl.GetComponent<GameControl>().RemoveVisibleFace(this.faceIndex);
+                
+            }
+        }
+    }
+
+        void Awake()
+    {
+        gameControl = GameObject.Find("GameControl");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
