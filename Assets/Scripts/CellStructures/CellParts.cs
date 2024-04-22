0f23cb610ff487dde77cc7afaaa355cb4fdc0309
orangeflowersn <orangeflowersn@gmail.com>
2024-04-22 22:55:50 +0800

    delete

--- Assets/Scripts/CellStructures/CellParts.cs
+++
@@ -1,9 +0,0 @@
-public enum CellParts
-{
-    Nucleus,
-    GolgiComplex,
-    Mitochondria,
-    Lysosome,
-    CellMembrane,
-    EndoplasmicReticulum
-}
--- Assets/Scripts/CentralDogma/Polypeptide.cs
+++
@@ -1,8 +0,0 @@
-public enum Polypeptide
-{
-    None,
-    Methionine,
-    Isoleucine,
-    Serine,
-    Tryptophan
-}
