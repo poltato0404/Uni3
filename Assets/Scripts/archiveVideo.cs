d98aeadaa3a3ecdef2ac158e054cbffb463f1d7b
orangeflowersn <orangeflowersn@gmail.com>
2024-04-23 22:33:34 +0800

    3D Button

--- Assets/Scripts/archiveVideo.cs
+++ Assets/Scripts/archiveVideo.cs
@@ -1,73 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video; // Import the Video module
 
-public class archiveVideo : MonoBehaviour
+public class ArchiveVideo : MonoBehaviour
 {
-    [SerializeField] GameObject video1, video2, video3, video4, video5, video6, video7, video8, video9; 
-    [SerializeField] List<GameObject> buttons; 
+    [SerializeField] GameObject video1, video2, video3, video4, video5, video6, video7, video8, video9;
+    [SerializeField] List<GameObject> buttons;
+    [SerializeField] GameObject buttonForVideo1Panel;
+    [SerializeField] GameObject buttonForVideo2Panel;
+    [SerializeField] GameObject panelForVideo1Info;
+    [SerializeField] GameObject panelForVideo2Info;
+
+    // Declare VideoPlayer variables for each video
+    VideoPlayer videoPlayer1;
+    VideoPlayer videoPlayer2;
+
     // Start is called before the first frame update
-    void Start(){
-        
+    void Start()
+    {
+        // Get VideoPlayer components from the video GameObjects
+        videoPlayer1 = video1.GetComponent<VideoPlayer>();
+        videoPlayer2 = video2.GetComponent<VideoPlayer>();
     }
-    public void playVideo1()
+
+    public void PlayVideo1()
     {
         video1.SetActive(true);
-        disableAllButtons();
+        videoPlayer1.Play(); // Play video 1
+        DisableAllButtons();
+        buttonForVideo1Panel.SetActive(true);
     }
 
-    public void playVideo2()
+    public void PlayVideo2()
     {
         video2.SetActive(true);
-        disableAllButtons();
+        videoPlayer2.Play(); // Play video 2
+        DisableAllButtons();
+        buttonForVideo2Panel.SetActive(true);
     }
 
-    public void playVideo3()
+    public void PlayVideo3()
     {
         video3.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
 
-    public void playVideo4()
+    public void PlayVideo4()
     {
         video4.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
 
-    public void playVideo5()
+    public void PlayVideo5()
     {
         video5.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
 
-    public void playVideo6()
+    public void PlayVideo6()
     {
         video6.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
 
-      public void playVideo7()
+    public void PlayVideo7()
     {
         video7.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
 
-      public void playVideo8()
+    public void PlayVideo8()
     {
         video8.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
 
-     public void playVideo9()
+    public void PlayVideo9()
     {
         video9.SetActive(true);
-        disableAllButtons();
+        DisableAllButtons();
     }
-    
 
-    void disableAllButtons(){
-        for(int i = 0 ; i < buttons.Count; i++ ){
-            buttons[i].SetActive(false);
+    void DisableAllButtons()
+    {
+        foreach (var button in buttons)
+        {
+            button.SetActive(false);
         }
+    }
+
+    public void ShowVideo1Panel()
+    {
+        panelForVideo1Info.SetActive(true);
+        buttonForVideo1Panel.SetActive(false); // Disable button after clicking
+        videoPlayer1.Stop(); // Stop video 1
+    }
+
+    public void ShowVideo2Panel()
+    {
+        panelForVideo2Info.SetActive(true);
+        buttonForVideo2Panel.SetActive(false); // Disable button after clicking
+        videoPlayer2.Stop(); // Stop video 2
     }
 }
