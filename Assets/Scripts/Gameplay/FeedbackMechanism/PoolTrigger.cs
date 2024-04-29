853703e97b954fbe8de551657518ac21528000bf
reirei <zelsevilleta@gmail.com>
2024-04-29 08:54:17 +0800

    Pahabol na bugs
    mima im so tired

--- Assets/Scripts/Gameplay/FeedbackMechanism/PoolTrigger.cs
+++ Assets/Scripts/Gameplay/FeedbackMechanism/PoolTrigger.cs
@@ -1,18 +1,18 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class PoolTrigger : MonoBehaviour
-// {
-//     public Transform point;
-//     public int score;
+public class PoolTrigger1 : MonoBehaviour
+{
+    public Transform point;
+    public int score;
 
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             other.transform.position = point.position;
-//             QuestManager.Instance.UpdateScore(-score);
-//         }
-//     }
-// }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = point.position;
+            QuestManager.Instance.UpdateScore(-score);
+        }
+    }
+}
