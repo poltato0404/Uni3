5ae2a9b18f6c6f902aacb4bc2c6342d5553f5ec9
orangeflowersn <orangeflowersn@gmail.com>
2024-05-14 00:28:44 +0800

    rrrrr

--- Assets/objectivesManager.cs
+++ Assets/objectivesManager.cs
@@ -22,11 +22,11 @@ public class objectivesManager : MonoBehaviour, IDataPersistence
     public TextMeshProUGUI minor3;
     public int stringCountEvidence;
     public PlayerBehaviour playerBehaviour;
-    
+
 
     public void SaveData(ref GameData data)
     {
-        
+
 
     }
 
@@ -35,47 +35,50 @@ public class objectivesManager : MonoBehaviour, IDataPersistence
         currentLevel = data.currentLevel;
         levelText.text = "Level " + 1;
         Main2.text = "- Find all the documents in the level";
-        Main3.text = "- Find all the coins in the level(Optional)";
-       
+        Main3.text = "- Find all the coins in the level (Optional)";
+
 
     }
 
     void Update()
     {
 
-       isLaptopRetrieved = mapL.gotLaptop; 
-       if(isLaptopRetrieved){
-       minor1.text = "<s>(1/1)</s>";
-       Main1.text = "<s>- Find a Laptop that belongs to Dr. Doe</s>"; 
-       }
-       
-       else{ 
-         minor1.text = "(0/1)";
-        Main1.text = "- Find a Laptop that belongs to Dr. Doe"; }
+        isLaptopRetrieved = mapL.gotLaptop;
+        if (isLaptopRetrieved)
+        {
+            minor1.text = "<s>(1/1)</s>";
+            Main1.text = "<s>- Find a Laptop that belongs to Dr. Doe</s>";
+        }
+
+        else
+        {
+            minor1.text = "(0/1)";
+            Main1.text = "- Find a Laptop that belongs to Dr. Doe";
+        }
 
 
-       minor2.text = "("+ playerBehaviour.stringEvidenceCount +"/3)";
-       if (playerBehaviour.stringEvidenceCount == 3)
-       {
-        Main2.text = "<s>- Find all the documents in the level</s>";
-        minor2.text = "(3/3)";
-       }
-       
+        minor2.text = "(" + playerBehaviour.stringEvidenceCount + "/3)";
+        if (playerBehaviour.stringEvidenceCount == 3)
+        {
+            Main2.text = "<s>- Find all the documents in the level</s>";
+            minor2.text = "(3/3)";
+        }
 
-       switch(currentLevel)
-       {
-        case 1: 
-             minor3.text = "("+ playerBehaviour.coinsCollected +"/3)";
-            break;
 
-        case 2:
-             minor3.text = "("+ playerBehaviour.coinsCollected +"/5)";
-            break;
+        switch (currentLevel)
+        {
+            case 1:
+                minor3.text = "(" + playerBehaviour.coinsCollected + "/3)";
+                break;
 
-        case 3:
-             minor3.text = "("+ playerBehaviour.coinsCollected +"/7)";
-            break;
-       }
+            case 2:
+                minor3.text = "(" + playerBehaviour.coinsCollected + "/5)";
+                break;
+
+            case 3:
+                minor3.text = "(" + playerBehaviour.coinsCollected + "/7)";
+                break;
+        }
 
     }
 }
